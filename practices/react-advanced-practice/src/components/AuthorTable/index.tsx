7144374f339d183b7971f639d6e4b6a9a2280896
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-17 17:15:17 +0700

    feat: fix re render for component AuthorTable

--- practices/react-advanced-practice/src/components/AuthorTable/index.tsx
+++ practices/react-advanced-practice/src/components/AuthorTable/index.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 // Import radix ui
 import { Table } from '@radix-ui/themes';
 
@@ -17,38 +19,38 @@ export interface AuthorTableProps {
 }
 
 const AuthorTable = ({ authors, onEditAuthor, onDeleteAuthor }: AuthorTableProps) => {
-  const renderTableHeaderContent = () =>
-    TABLE_TITLES.map((title) => (
-      <Table.ColumnHeaderCell key={title} className="py-3.5 text-sm">
-        {title}
-      </Table.ColumnHeaderCell>
-    ));
+  const renderTableHeaderContent = useMemo(
+    () =>
+      TABLE_TITLES.map((title) => (
+        <Table.ColumnHeaderCell key={title} className="py-3.5 text-sm">
+          {title}
+        </Table.ColumnHeaderCell>
+      )),
+    [],
+  );
 
-  const renderTableBodyContent = (
-    authors: Author[],
-    onEditAuthor: (updatedAuthor: Author) => void,
-    onDeleteAuthor: (id: string) => void,
-  ) =>
-    authors.map((author) => (
-      <AuthorItem
-        key={author.id}
-        author={author}
-        onEdit={onEditAuthor}
-        onDelete={onDeleteAuthor}
-      />
-    ));
+  const renderTableBodyContent = useMemo(
+    () =>
+      authors.map((author) => (
+        <AuthorItem
+          key={author.id}
+          author={author}
+          onEdit={onEditAuthor}
+          onDelete={onDeleteAuthor}
+        />
+      )),
+    [authors, onEditAuthor, onDeleteAuthor],
+  );
 
   return (
     <Table.Root className="w-full">
       <Table.Header>
         <Table.Row className="font-bold text-base">
-          {renderTableHeaderContent()}
+          {renderTableHeaderContent}
           <Table.ColumnHeaderCell className="w-32 py-3.5"></Table.ColumnHeaderCell>
         </Table.Row>
       </Table.Header>
-      <Table.Body>
-        {renderTableBodyContent(authors, onEditAuthor, onDeleteAuthor)}
-      </Table.Body>
+      <Table.Body>{renderTableBodyContent}</Table.Body>
     </Table.Root>
   );
 };
