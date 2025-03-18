19786c1caf25b231b69a21c1953a0956eca3843f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-18 17:42:09 +0700

    feat: fix re-render for components Sidebar

--- practices/react-advanced-practice/src/components/Sidebar/index.tsx
+++ practices/react-advanced-practice/src/components/Sidebar/index.tsx
@@ -1,5 +1,11 @@
 import { memo, useMemo, ReactElement } from 'react';
 
+// Import radix
+import { Box, Flex } from '@radix-ui/themes';
+
+// Import components common
+import { Logo } from '@/components/common';
+
 // Import constants
 import { SIDEBAR_ITEMS } from '@/constants';
 
@@ -8,6 +14,12 @@ import SidebarItem from './SidebarItem';
 
 // Import help box
 import HelpBox from './HelpBox';
+
+// Import zustand store
+import { useThemeStore } from '@/stores';
+
+// Import types
+import { ThemeMode } from '@/types';
 
 interface SidebarItemType {
   icon: ReactElement;
@@ -18,6 +30,8 @@ interface SidebarItemType {
 }
 
 const Sidebar = () => {
+  const { theme } = useThemeStore();
+
   const mainItems: SidebarItemType[] = useMemo(
     () => SIDEBAR_ITEMS.filter((item) => !item.group),
     [],
@@ -41,14 +55,21 @@ const Sidebar = () => {
     ));
 
   return (
-    <>
+    <Box>
+      <Flex
+        justify="center"
+        align="center"
+        className="mb-[22px] gap-12 gradient-border pb-7"
+      >
+        <Logo color={theme === ThemeMode.Dark ? 'secondary' : 'primary'} href="/home" />
+      </Flex>
       <ul className="px-[30px]">
         {renderSidebarItems(mainItems)}
         <li className="font-bold py-6 px-4 dark:text-light">ACCOUNT PAGES</li>
         {renderSidebarItems(accountItems)}
       </ul>
       <HelpBox />
-    </>
+    </Box>
   );
 };
 
