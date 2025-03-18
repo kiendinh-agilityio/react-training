519db9dceeb8ea1a1e7a2ca28711ffbd6e309f69
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-18 17:45:20 +0700

    feat: fix re-render for components InputGroup

--- practices/react-advanced-practice/src/components/InputGroup/index.tsx
+++ practices/react-advanced-practice/src/components/InputGroup/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, memo } from 'react';
 
 // Import libs of Radix UI
 import { Flex } from '@radix-ui/themes';
@@ -20,36 +20,40 @@ interface InputGroupProps {
   onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-const InputGroup = forwardRef<HTMLInputElement, InputGroupProps>(
-  (
-    {
-      label,
-      name,
-      type = 'text',
-      value,
-      placeholder = '',
-      errorMessage,
-      onChange,
-      onBlur,
-    }: InputGroupProps,
-    ref,
-  ) => (
-    <Flex direction="column" className="gap-1.5">
-      <Text size={TextSize.Base} as="label" className="text-dark">
-        {label}
-      </Text>
-      <Input
-        ref={ref}
-        name={name}
-        type={type}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-        errorMessage={errorMessage}
-        className="dark:bg-light"
-      />
-    </Flex>
+const InputGroup = memo(
+  forwardRef<HTMLInputElement, InputGroupProps>(
+    (
+      {
+        label,
+        name,
+        type = 'text',
+        value,
+        placeholder = '',
+        errorMessage,
+        onChange,
+        onBlur,
+      },
+      ref,
+    ) => (
+      <Flex direction="column" className="gap-1.5">
+        {label && (
+          <Text size={TextSize.Base} as="label" className="text-dark">
+            {label}
+          </Text>
+        )}
+        <Input
+          ref={ref}
+          name={name}
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          errorMessage={errorMessage}
+          className="dark:bg-light"
+        />
+      </Flex>
+    ),
   ),
 );
 
