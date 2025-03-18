71725cad22f96fb1cac1a7a9e96fd7de00b571d8
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-18 17:39:51 +0700

    feat: fix re-render for components AuthorForm

--- practices/react-advanced-practice/src/components/AuthorForm/index.tsx
+++ practices/react-advanced-practice/src/components/AuthorForm/index.tsx
@@ -1,3 +1,5 @@
+import { memo, useCallback, useMemo } from 'react';
+
 // Import react hook form
 import { useForm, Controller } from 'react-hook-form';
 
@@ -35,200 +37,201 @@ interface AuthorsFormProps {
   onSubmit: () => void;
 }
 
-const AuthorForm = ({
-  isUpdate,
-  selectedAuthor,
-  closeModal,
-  onChange,
-  onSubmit,
-}: AuthorsFormProps) => {
-  const {
-    control,
-    handleSubmit,
-    setValue,
-    getValues,
-    formState: { errors, isDirty, dirtyFields },
-  } = useForm<Author>({
-    defaultValues: selectedAuthor,
-    mode: 'onBlur',
-  });
+const AuthorForm = memo(
+  ({ isUpdate, selectedAuthor, closeModal, onChange, onSubmit }: AuthorsFormProps) => {
+    const {
+      control,
+      handleSubmit,
+      setValue,
+      getValues,
+      formState: { errors, isDirty, dirtyFields },
+    } = useForm<Author>({
+      defaultValues: selectedAuthor,
+      mode: 'onBlur',
+    });
 
-  const handleFormAction = (name: keyof Author, value: string) => {
-    setValue(name, value, { shouldValidate: true, shouldDirty: true });
-    onChange({ ...getValues(), [name]: value });
-  };
+    const handleFormAction = useCallback(
+      (name: keyof Author, value: string) => {
+        setValue(name, value, { shouldValidate: true, shouldDirty: true });
+        onChange({ ...getValues(), [name]: value });
+      },
+      [setValue, getValues, onChange],
+    );
 
-  // Handle input fields change
-  const handleFieldChange =
-    (name: keyof Author) =>
-    (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-      const value = e.target.value;
-      handleFormAction(name, value);
-    };
+    // Handle input fields change
+    const handleFieldChange = useCallback(
+      (name: keyof Author) =>
+        (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+          const value = e.target.value;
+          handleFormAction(name, value);
+        },
+      [handleFormAction],
+    );
 
-  const handleValidateDate = (value: string): string | boolean =>
-    validateDate(value, DATE_FORMAT.SECONDARY) || MESSAGE_ERROR.INVALID_DATE;
+    const handleValidateDate = useCallback(
+      (value: string): string | boolean =>
+        validateDate(value, DATE_FORMAT.SECONDARY) || MESSAGE_ERROR.INVALID_DATE,
+      [],
+    );
 
-  const handleCheckIsDisabled = (
-    isUpdate: boolean,
-    isDirty: boolean,
-    dirtyFields: Record<string, boolean>,
-  ): boolean => {
-    return isUpdate && !isDirty && Object.keys(dirtyFields).length === 0;
-  };
+    const handleCheckIsDisabled = useMemo(
+      () => isUpdate && !isDirty && Object.keys(dirtyFields).length === 0,
+      [isUpdate, isDirty, dirtyFields],
+    );
 
-  return (
-    <>
-      <Heading
-        as="h2"
-        text={`${isUpdate ? 'EDIT' : 'ADD'} AUTHOR`}
-        className="font-bold text-center"
-      />
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
-        <Controller
-          name="name"
-          control={control}
-          rules={{
-            required: MESSAGE_ERROR.REQUIRED_ERROR(PROFILE_AUTHOR.NAME),
-            pattern: {
-              value: REGEX.NAME,
-              message: MESSAGE_ERROR.INVALID_NAME,
-            },
-          }}
-          render={({ field }) => (
-            <InputGroup
-              label="Name"
-              {...field}
-              type="text"
-              placeholder="Please enter name"
-              errorMessage={errors.name?.message}
-              onChange={handleFieldChange('name')}
-              onBlur={handleFieldChange('name')}
-            />
-          )}
+    return (
+      <>
+        <Heading
+          as="h2"
+          text={`${isUpdate ? 'EDIT' : 'ADD'} AUTHOR`}
+          className="font-bold text-center"
         />
-        <Controller
-          name="email"
-          control={control}
-          rules={{
-            required: MESSAGE_ERROR.REQUIRED_ERROR(PROFILE_AUTHOR.EMAIL),
-            pattern: {
-              value: REGEX.EMAIL,
-              message: MESSAGE_ERROR.INVALID_EMAIL,
-            },
-          }}
-          render={({ field }) => (
-            <InputGroup
-              label="Email"
-              {...field}
-              type="email"
-              placeholder="Please enter email address"
-              errorMessage={errors.email?.message}
-              onChange={handleFieldChange('email')}
-              onBlur={handleFieldChange('email')}
-            />
-          )}
-        />
-        <Controller
-          name="avatarUrl"
-          control={control}
-          rules={{
-            required: MESSAGE_ERROR.REQUIRED_ERROR(PROFILE_AUTHOR.AVATAR_URL),
-            pattern: {
-              value: REGEX.AVATAR_URL,
-              message: MESSAGE_ERROR.INVALID_AVATAR_URL,
-            },
-          }}
-          render={({ field }) => (
-            <InputGroup
-              label="Avatar"
-              {...field}
-              type="text"
-              placeholder="Please enter link image"
-              errorMessage={errors.avatarUrl?.message}
-              onChange={handleFieldChange('avatarUrl')}
-              onBlur={handleFieldChange('avatarUrl')}
-            />
-          )}
-        />
-        <Controller
-          name="date"
-          control={control}
-          rules={{
-            validate: handleValidateDate,
-          }}
-          render={({ field }) => (
-            <InputGroup
-              label="Employed"
-              {...field}
-              type="date"
-              value={field.value || today}
-              errorMessage={errors.date?.message}
-              onChange={handleFieldChange('date')}
-              onBlur={handleFieldChange('date')}
-            />
-          )}
-        />
-        <Grid columns="3" className="gap-5">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
           <Controller
-            name="roles"
+            name="name"
             control={control}
+            rules={{
+              required: MESSAGE_ERROR.REQUIRED_ERROR(PROFILE_AUTHOR.NAME),
+              pattern: {
+                value: REGEX.NAME,
+                message: MESSAGE_ERROR.INVALID_NAME,
+              },
+            }}
             render={({ field }) => (
-              <Select
-                label="Roles"
+              <InputGroup
+                label="Name"
                 {...field}
-                optionsList={ROLES}
-                onChange={handleFieldChange('roles')}
-                onBlur={handleFieldChange('roles')}
+                type="text"
+                placeholder="Please enter name"
+                errorMessage={errors.name?.message}
+                onChange={handleFieldChange('name')}
+                onBlur={handleFieldChange('name')}
               />
             )}
           />
           <Controller
-            name="position"
+            name="email"
             control={control}
+            rules={{
+              required: MESSAGE_ERROR.REQUIRED_ERROR(PROFILE_AUTHOR.EMAIL),
+              pattern: {
+                value: REGEX.EMAIL,
+                message: MESSAGE_ERROR.INVALID_EMAIL,
+              },
+            }}
             render={({ field }) => (
-              <Select
-                label="Positions"
+              <InputGroup
+                label="Email"
                 {...field}
-                optionsList={POSITIONS}
-                onChange={handleFieldChange('position')}
-                onBlur={handleFieldChange('position')}
+                type="email"
+                placeholder="Please enter email address"
+                errorMessage={errors.email?.message}
+                onChange={handleFieldChange('email')}
+                onBlur={handleFieldChange('email')}
               />
             )}
           />
           <Controller
-            name="status"
+            name="avatarUrl"
             control={control}
+            rules={{
+              required: MESSAGE_ERROR.REQUIRED_ERROR(PROFILE_AUTHOR.AVATAR_URL),
+              pattern: {
+                value: REGEX.AVATAR_URL,
+                message: MESSAGE_ERROR.INVALID_AVATAR_URL,
+              },
+            }}
             render={({ field }) => (
-              <Select
-                label="Status"
+              <InputGroup
+                label="Avatar"
                 {...field}
-                optionsList={STATUS}
-                onChange={handleFieldChange('status')}
-                onBlur={handleFieldChange('status')}
+                type="text"
+                placeholder="Please enter link image"
+                errorMessage={errors.avatarUrl?.message}
+                onChange={handleFieldChange('avatarUrl')}
+                onBlur={handleFieldChange('avatarUrl')}
               />
             )}
           />
-        </Grid>
-        <Flex justify="center" align="center" className="gap-6 mt-6">
-          <Button
-            onClick={handleSubmit(onSubmit)}
-            isDisabled={handleCheckIsDisabled(isUpdate, isDirty, dirtyFields)}
-            className="w-[150px]"
-          >
-            {isUpdate ? 'Save' : 'Submit'}
-          </Button>
-          <Button
-            variant={ButtonVariant.Tertiary}
-            onClick={closeModal}
-            className="w-[150px]"
-          >
-            Cancel
-          </Button>
-        </Flex>
-      </form>
-    </>
-  );
-};
+          <Controller
+            name="date"
+            control={control}
+            rules={{
+              validate: handleValidateDate,
+            }}
+            render={({ field }) => (
+              <InputGroup
+                label="Employed"
+                {...field}
+                type="date"
+                value={field.value || today}
+                errorMessage={errors.date?.message}
+                onChange={handleFieldChange('date')}
+                onBlur={handleFieldChange('date')}
+              />
+            )}
+          />
+          <Grid columns="3" className="gap-5">
+            <Controller
+              name="roles"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  label="Roles"
+                  {...field}
+                  optionsList={ROLES}
+                  onChange={handleFieldChange('roles')}
+                  onBlur={handleFieldChange('roles')}
+                />
+              )}
+            />
+            <Controller
+              name="position"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  label="Positions"
+                  {...field}
+                  optionsList={POSITIONS}
+                  onChange={handleFieldChange('position')}
+                  onBlur={handleFieldChange('position')}
+                />
+              )}
+            />
+            <Controller
+              name="status"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  label="Status"
+                  {...field}
+                  optionsList={STATUS}
+                  onChange={handleFieldChange('status')}
+                  onBlur={handleFieldChange('status')}
+                />
+              )}
+            />
+          </Grid>
+          <Flex justify="center" align="center" className="gap-6 mt-6">
+            <Button
+              onClick={handleSubmit(onSubmit)}
+              isDisabled={handleCheckIsDisabled}
+              className="w-[150px]"
+            >
+              {isUpdate ? 'Save' : 'Submit'}
+            </Button>
+            <Button
+              variant={ButtonVariant.Tertiary}
+              onClick={closeModal}
+              className="w-[150px]"
+            >
+              Cancel
+            </Button>
+          </Flex>
+        </form>
+      </>
+    );
+  },
+);
 
 export default AuthorForm;
