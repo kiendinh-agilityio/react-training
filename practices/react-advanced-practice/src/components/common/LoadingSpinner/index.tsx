9a578c47a71b36eb5d5e64add914198bdb64fcb3
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-18 17:43:09 +0700

    feat: fix re-render for components LoadingSpinner

--- practices/react-advanced-practice/src/components/common/LoadingSpinner/index.tsx
+++ practices/react-advanced-practice/src/components/common/LoadingSpinner/index.tsx
@@ -1,13 +1,15 @@
+import { memo } from 'react';
+
 // Import radix ui
 import { Box } from '@radix-ui/themes';
 
 // Import styles animation for toast
 import './loading.css';
 
-const LoadingSpinner = () => (
+const LoadingSpinner = memo(() => (
   <Box className="loading-container">
     <Box as="span" className="loading-indicator"></Box>
   </Box>
-);
+));
 
 export default LoadingSpinner;
