8a6105c9a295610d37c2cefc11c8e06575b6081c
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-26 17:14:59 +0700

    feat: use persist of zustand for useThemeStore

--- practices/react-advanced-practice/src/stores/useThemeStore.ts
+++ practices/react-advanced-practice/src/stores/useThemeStore.ts
@@ -1,22 +1,28 @@
 import { create } from 'zustand';
 
+import { persist } from 'zustand/middleware';
+
 // Import types
 import { ThemeMode } from '@/types';
 
 interface ThemeStore {
   theme: string;
   setTheme: (theme: string) => void;
+  toggleTheme: () => void;
 }
 
-export const useThemeStore = create<ThemeStore>((set) => {
-  const savedTheme = localStorage.getItem('theme') || ThemeMode.Light;
-
-  return {
-    theme: savedTheme,
-    setTheme: (theme) => {
-      localStorage.setItem('theme', theme);
-
-      set({ theme });
+export const useThemeStore = create<ThemeStore>()(
+  persist(
+    (set) => ({
+      theme: ThemeMode.Light,
+      setTheme: (theme) => set({ theme }),
+      toggleTheme: () =>
+        set((state) => ({
+          theme: state.theme === ThemeMode.Light ? ThemeMode.Dark : ThemeMode.Light,
+        })),
+    }),
+    {
+      name: 'theme-preference',
     },
-  };
-});
+  ),
+);
