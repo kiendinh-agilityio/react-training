21741f6b0e9f7719fad2d210987ac9aa500ba402
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-27 14:15:29 +0700

    feat: update concept setQueryData of React Query for Home

--- practices/react-advanced-practice/src/pages/Home/index.tsx
+++ practices/react-advanced-practice/src/pages/Home/index.tsx
@@ -71,8 +71,11 @@ const Home = () => {
   // Use mutation for adding a new author
   const { mutate: addAuthor, isPending: isAdding } = useMutation({
     mutationFn: addNewAuthor,
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: [QueryKey.Authors] });
+    onSuccess: async (newAuthor) => {
+      queryClient.setQueryData([QueryKey.Authors], (oldAuthors: Author[] = []) => [
+        ...oldAuthors,
+        newAuthor,
+      ]);
 
       handleShowToast(MESSAGE_SUCCESS.ADD_AUTHOR, Notification.Success);
     },
@@ -88,8 +91,13 @@ const Home = () => {
     { id: string; author: Author }
   >({
     mutationFn: ({ id, author }) => editAuthorById(id, author),
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: [QueryKey.Authors] });
+    onSuccess: (updatedAuthor) => {
+      // Update the query data directly instead of invalidating
+      queryClient.setQueryData<Author[]>([QueryKey.Authors], (oldAuthors = []) =>
+        oldAuthors.map((author) =>
+          author.id === updatedAuthor.id ? updatedAuthor : author,
+        ),
+      );
 
       handleShowToast(MESSAGE_SUCCESS.EDIT_AUTHOR, Notification.Success);
     },
@@ -99,10 +107,17 @@ const Home = () => {
   });
 
   // Use mutation for delete an author
-  const { mutate: deleteAuthor, isPending: isDeleting } = useMutation({
+  const { mutate: deleteAuthor, isPending: isDeleting } = useMutation<
+    Author,
+    Error,
+    string
+  >({
     mutationFn: deleteAuthorById,
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: [QueryKey.Authors] });
+    onSuccess: (_, deletedAuthorId) => {
+      // Update the query data directly instead of invalidating
+      queryClient.setQueryData<Author[]>([QueryKey.Authors], (oldAuthors = []) =>
+        oldAuthors.filter((author) => author.id !== deletedAuthorId),
+      );
 
       handleShowToast(MESSAGE_SUCCESS.DELETE_AUTHOR, Notification.Success);
     },
